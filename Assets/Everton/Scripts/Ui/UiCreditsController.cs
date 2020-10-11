cbe4bb9bae69b348d3317f63ee53dca139734efa
EvertonBorges <evertonsoaresborges@gmail.com>
2020-10-11 11:52:11 -0400

    feat - Control Menu and New Sounds

--- Assets/Everton/Scripts/Ui/UiCreditsController.cs
+++ Assets/Everton/Scripts/Ui/UiCreditsController.cs
@@ -3,21 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UiCreditsController : MonoBehaviour
+public class UiCreditsController : UiGenericMenu
 {
 
-    [SerializeField] private UiMainMenuController uiMainMenuController;
-    [SerializeField] private Button buttonClose;
-    [SerializeField] private UiFadeEffect bgFadeEffect;
-
-    void Awake() {
-        buttonClose.onClick.RemoveAllListeners();
-
-        buttonClose.onClick.AddListener(ButtonClose);
-    }
-
-    public void Show() => bgFadeEffect.FadeIn();
-
-    private void ButtonClose() => bgFadeEffect.FadeOut();
 
 }
--- Assets/Everton/Scripts/Ui/UiMainMenuController.cs
+++ Assets/Everton/Scripts/Ui/UiMainMenuController.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class UiMainMenuController : MonoBehaviour
 {
 
+    [Header("Buttons")]
     [SerializeField] private Button buttonPlay;
     [SerializeField] private Button buttonControls;
     [SerializeField] private Button buttonCredits;
     [SerializeField] private Button buttonExitGame;
     [SerializeField] private Button buttonSettings;
 
+    [Header("Background")]
     [SerializeField] private UiFadeEffect fadeEffect;
 
+    [Header("Menus")]
+    [SerializeField] private UiControlsController uiControlsController;
     [SerializeField] private UiCreditsController uiCreditsController;
+    [SerializeField] private UiSettingsController uiSettingsController;
+
+    private AudioSource _audioSource;
 
     void Awake()
     {
+        _audioSource = GetComponent<AudioSource>();
         fadeEffect.GetComponent<CanvasGroup>().alpha = 1f;
         StartCoroutine(CustomWait.Wait(1f, () => fadeEffect.FadeOut()));
 
@@ -34,23 +43,18 @@ public class UiMainMenuController : MonoBehaviour
         buttonSettings.onClick.AddListener(ButtonSettings);
     }
 
-    private void ButtonPlay() => fadeEffect.FadeIn(() => {
-        SceneController.ToGame();
-        MainMusicController.Instance.PlayGameAudioClip();
-    });
-
-    private void ButtonControls()
+    private void ButtonPlay() 
     {
-        Debug.Log("Button Controls");
+        _audioSource.Play();
+        fadeEffect.FadeIn(() => {
+            SceneController.ToGame();
+            MainMusicController.Instance.PlayGameAudioClip();
+        });
     }
 
+    private void ButtonControls() => uiControlsController.Show();
     private void ButtonCredits() => uiCreditsController.Show();
-
     private void ButtonExitGame() => SceneController.Exit();
-
-    private void ButtonSettings()
-    {
-        Debug.Log("Button Settings");
-    }
+    private void ButtonSettings() => uiSettingsController.Show();
 
 }
